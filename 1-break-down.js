b0e7b4e941e457b7eaa5f2c81b558db7a843d6a7
LudovicGouverneur <ludovic.gouverneur@gmail.com>
2018-08-07 11:27:53 +0200

    Update 1-break-down.js

--- 1-break-down.js
+++ 1-break-down.js
@@ -1,13 +1,14 @@
 let input = 'table';
-2	let expected = 'bee';
-3	let output;
-4	
-5	console.log("'a string'.charAt(2) + 'ee';");
-6	console.log("- " + input + " should become " + expected);
-7	 
-8	{ //  (sr, "table") -> (str, "bee")
-9	    const step_1 = charAt(input); 
-10	    // str: "b"
-11	    const step_2 = step_1 + "ee"; 
-12	    // str: "bee"
-13	};
+let expected = 'bee';
+let output;
+
+console.log(input +".charAt(2) + 'ee';");
+console.log("- " + input + " should become " + expected);
+ 
+{ //  (sr, "table") -> (str, "bee")
+    const step_1 = input.charAt(2); 
+    // str: "b"
+    const step_2 = step_1 + "ee"; 
+    // str: "bee"
+    output = step_2;
+};
